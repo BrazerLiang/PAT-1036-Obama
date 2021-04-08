c9bab48ec21ab5cd3340ee29bfc3d3f9dc9e26e6
brazer_glb <andysl9@163.com>
2021-04-08 15:45:58 +0800

    添加项目文件。

---
+++ PAT 1036 Obama.cpp
@@ -0,0 +1,30 @@
+﻿#include<cstdio>
+int main()
+{
+	int row, col;
+	char ch;
+	int i = 0;
+	int k = 0;
+	scanf_s("%d", &col);
+	getchar();
+	scanf_s("%c", &ch);
+	if (col % 2 == 0)k = 2;
+	else k = 1;
+	for (i = 0; i < col; i++)
+	{
+		if (i == 0 || i == col - k)
+		{
+			for (int k = 0; k < col; k++)
+				printf("%c", ch);
+			printf("\n");
+		}
+		else if (i % 2 == 0) {
+			printf("%c", ch);
+			for (int k = 1; k < col - 1; k++)
+				printf(" ");
+			printf("%c", ch);
+			printf("\n");
+		}
+	}
+	return 0;
+}
